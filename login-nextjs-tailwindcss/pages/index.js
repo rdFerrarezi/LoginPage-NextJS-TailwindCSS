a537d8b6602905c324970ef03b08798101f93945
R.Ferrarezzi <canalferrarezzi@gmail.com>
2022-02-02 19:46:29 -0300

    End project

--- login-nextjs-tailwindcss/pages/index.js
+++ login-nextjs-tailwindcss/pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
-import { FaFacebook, FaLinkedinIn, FaGoogle, FaFacebookF } from 'react-icons/fa';
+import { FaLinkedinIn, FaGoogle, FaFacebookF, FaRegEnvelope } from 'react-icons/fa';
+import { MdLockOutline } from 'react-icons/md';
+
 
 export default function Home() {
   return (
@@ -19,7 +21,7 @@ export default function Home() {
               <h2 className='text-3xl font-bold text-green-500 mb-2'>
                 Sign in to Account
               </h2>
-              <div className='border-2 w-10 border-green-500 inline-block mb-2'></div>
+              <div className='bg-green-500 border-2 w-10 border-green-500 inline-block mb-2'></div>
               <div className='flex justify-center my-2'>
                 <a href="#" className='border-2 border-gray-200 rounded-full p-3 mx-1'>
                   <FaFacebookF className='text-sm'/>
@@ -30,13 +32,32 @@ export default function Home() {
                 <a href="#" className='border-2 border-gray-200 rounded-full p-3 mx-1'>
                   <FaGoogle className='text-sm'/>
                 </a>
-              </div>
+              </div>{/* Social login section */}
               <p className='text-gray-400 my-3'>or use your email account</p>
+              <div className='flex flex-col items-center'>
+                <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
+                  <FaRegEnvelope className='text-gray-400 mr-2'/>
+                  <input type='email' name='email' placeholder='Email' className='bg-gray-100 outline-none text-sm flex-1'/>
+                </div>
+                <div className='bg-gray-100 w-64 p-2 flex items-center mb-3'>
+                  <MdLockOutline className='text-gray-400 mr-2'/>
+                  <input type='password' name='password' placeholder='Password' className='bg-gray-100 outline-none text-sm flex-1'/>
+                </div>
+                <div className='flex justify-between w-64 mb-5'>
+                  <label className='flex items-center text-xs'>
+                    <input type='checkbox' name='remember' className='mr-1'/>
+                    Remember me
+                  </label>
+                  <a href="#" className='text-xs'>Forgot Password?</a>
+                </div>
+                <a href="#" className='border-2 text-green-500 border-green-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-green-500 hover:text-white'>Sign In</a>
+              </div>
             </div>
           </div>{/* Sign in section */}
+          
           <div className='w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12'>
             <h2 className='text-3xl font-bold mb-2'>Hello Friend</h2>
-            <div className='border-2 w-10 border-white inline-block mb-2'></div>        
+            <div className='bg-white border-2 w-10 border-white inline-block mb-2'></div>        
             <p className='mb-2'>
               Fill up personal information and start  journey  with us.
             </p>
